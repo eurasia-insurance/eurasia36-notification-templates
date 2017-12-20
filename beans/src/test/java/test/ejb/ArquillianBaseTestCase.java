a234a0f715bd0a114ac1e247e305a69853177f38
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-20 11:34:05 +0600

    module

---
+++ beans/src/test/java/test/ejb/ArquillianBaseTestCase.java
@@ -0,0 +1,29 @@
+package test.ejb;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.runner.RunWith;
+
+import tech.lapsa.eurasia36.notificationDaemon.template.beans.Eurasia36TemplateProviderBean;
+import tech.lapsa.lapsa.arquillian.archive.ArchiveBuilderFactory;
+
+@RunWith(Arquillian.class)
+public abstract class ArquillianBaseTestCase {
+
+    private static final Archive<?> DEPLOYMENT = ArchiveBuilderFactory.newEarBuilder() //
+	    .withModule(ArchiveBuilderFactory.newEjbBuilder() //
+		    .withPackageOf(Eurasia36TemplateProviderBean.class) //
+		    .withManifestFolder() //
+		    .build() //
+		    .dumpingTo(System.out::println)) //
+	    .withRuntimeDependencies()
+	    .build() //
+	    .dumpingTo(System.out::println) //
+	    .asEnterpriseArchive();
+
+    @Deployment(testable = true)
+    public static Archive<?> createDeploymentEAR() {
+	return DEPLOYMENT;
+    }
+}
